e82e3d98904e17a58f6f119e1a51b91eb3d72e4c
Duong Vu <vu.t.duong01@gmail.com>
2024-02-05 12:28:03 -0500

    Update register/login card

--- tmuclassifieds-client/src/components/LogInCard.jsx
+++ tmuclassifieds-client/src/components/LogInCard.jsx
@@ -6,29 +6,32 @@ const LogInCard = () => {
     const email = event.target[0].value;
     const password = event.target[1].value;
 
+    // TODO: Form validation
     console.log(email);
     console.log(password);
   };
 
   return (
-    <form onSubmit={logIn}>
-      <div>
-        <label htmlFor="inputEmail" className="form-label">Email address *</label>
-        <input type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" />
-      </div>
-      <br />
-      <div>
-        <label htmlFor="inputPassword" className="form-label">Password *</label>
-        <input type="password" className="form-control" id="inputPassword" />
-      </div>
-      <br />
-      <div className="form-check">
-        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-        <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
-      </div>
-      <br />
-      <button type="submit" className="btn btn-primary">Log In</button>
-    </form>
+    <div>
+      <form onSubmit={logIn}>
+        <div>
+          <label htmlFor="loginEmail" className="form-label">Email address *</label>
+          <input type="email" className="form-control" id="loginEmail" required />
+        </div>
+        <br />
+        <div>
+          <label htmlFor="loginPassword" className="form-label">Password *</label>
+          <input type="password" className="form-control" id="loginPassword" required />
+        </div>
+        <br />
+        <div className="form-check">
+          <input type="checkbox" className="form-check-input" id="rememberCheck" />
+          <label className="form-check-label" htmlFor="rememberCheck">Remember me</label>
+        </div>
+        <br />
+        <button type="submit" className="btn btn-primary">Log In</button>
+      </form>
+    </div>
   );
 };
 
--- tmuclassifieds-client/src/components/RegisterCard.jsx
+++ tmuclassifieds-client/src/components/RegisterCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import * as bootstrap from "bootstrap/dist/js/bootstrap.min";
 
 const RegisterCard = () => {
   const register = (event) => {
@@ -7,30 +8,51 @@ const RegisterCard = () => {
     const password = event.target[1].value;
     const passwordConfirm = event.target[2].value;
 
+    // TODO: Form validation
     console.log(email);
     console.log(password);
     console.log(passwordConfirm);
+
+    if (password !== passwordConfirm) {
+      bootstrap.Toast.getOrCreateInstance(document.getElementById("passwordWarningToast")).show();
+      return;
+    }
   };
 
   return (
-    <form onSubmit={register}>
-      <div>
-        <label htmlFor="inputEmail" className="form-label">Email address *</label>
-        <input type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" />
+    <div>
+      <form onSubmit={register}>
+        <div>
+          <label htmlFor="registerEmail" className="form-label">Email address *</label>
+          <input type="email" className="form-control" id="registerEmail" required/>
+        </div>
+        <br />
+        <div>
+          <label htmlFor="registerPassword" className="form-label">Password *</label>
+          <input type="password" className="form-control" id="registerPassword" required/>
+        </div>
+        <br />
+        <div>
+          <label htmlFor="registerConfirmPassword" className="form-label">Confirm password *</label>
+          <input type="password" className="form-control" id="registerConfirmPassword" required/>
+        </div>
+        <br />
+        <button type="submit" className="btn btn-primary">Register</button>
+      </form>
+
+      {/* https://getbootstrap.com/docs/5.3/components/toasts/ */}
+      <div className="toast-container position-fixed bottom-0 end-0">
+        <div id="passwordWarningToast" className="toast" role="alert" aria-live="assertive" aria-atomic="true">
+          <div className="toast-header">
+            <strong className="me-auto">Invalid passwords</strong>
+            <button type="button" className="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+          </div>
+          <div className="toast-body">
+              Please make sure the passswords match.
+          </div>
+        </div>
       </div>
-      <br />
-      <div>
-        <label htmlFor="inputPassword" className="form-label">Password *</label>
-        <input type="password" className="form-control" id="inputPassword" />
-      </div>
-      <br />
-      <div>
-        <label htmlFor="inputConfirmPassword" className="form-label">Confirm password *</label>
-        <input type="password" className="form-control" id="inputConfirmPassword" />
-      </div>
-      <br />
-      <button type="submit" className="btn btn-primary">Register</button>
-    </form>
+    </div>
   );
 };
 
